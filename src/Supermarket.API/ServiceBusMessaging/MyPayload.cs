4adb37d971cd1d32b6dfbdca0725184f7761ce48
himanshu <himanshu.mamodiya@infosys.com>
2020-01-28 05:30:02 +0000

    Code regarding SuperMarket

---
+++ src/Supermarket.API/ServiceBusMessaging/MyPayload.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceBusMessaging
+{
+    public class MyPayload
+    {
+        public bool Categories{ get; set; }
+        public int id { get; set; }
+        public var CategoriesOrProducts { get; set; }
+    }
+}
